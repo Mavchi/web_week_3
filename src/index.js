088784b33b225087d879db9b5493b711fd4779da
Aleksander Khlebnikov <aleksi.khlebnikov@gmail.com>
2022-09-23 06:15:54 +0000

    prints all needed data for exrcise 1 (week 3). municipality and population

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const dataTable = document.querySelector("table tbody")
+
+async function getData() {
+  const url1 = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff"
+  const dataPromise = await fetch(url1)
+  const dataJSON = await dataPromise.json()
+
+  //console.log(dataJSON.dataset.dimension.Alue.category.label)
+  let index = 0
+  for (let key in dataJSON.dataset.dimension.Alue.category.label) {
+    let tr = document.createElement("tr")
+    let td = document.createElement("td")
+    td.innerText = dataJSON.dataset.dimension.Alue.category.label[key]
+    tr.appendChild(td)
+
+    let td2 = document.createElement("td")
+    td2.innerText = dataJSON.dataset.value[index]
+    tr.appendChild(td2)
+
+    dataTable.appendChild(tr)
+    index += 1
+
+    //console.log(dataJSON.dataset.dimension.Alue.category.label[key])
+  }
+
+  /*
+  dataJSON.dataset.dimension.Alue.category.label.forEach( (country, index) => {
+    console.log(country)
+    console.log(dataJSON.dataset.value[index])
+  })
+  */
+}
+
+getData()
